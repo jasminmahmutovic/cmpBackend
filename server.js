32ec5ca79deef2affc774c819492c4970bff03ab
jasminmahmutovic <jasminm562@gmail.com>
2022-12-15 13:53:43 +0100

    Added some API calls

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+// process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 // import axios from "axios";
 const axios = require("axios");
 const cors = require("cors");
@@ -14,33 +15,75 @@ app.use(cors());
 app.use(express.json({ limit: "100mb" }));
 
 const PostDataToHydroCloud = async (req, res) => {
+	let url = "";
+
+	if (req.body.requestParameter) {
+		url = req.body.url + "/" + req.body.requestParameterValue;
+	} else {
+		url = req.body.url;
+	}
+
+	axios.defaults.headers["X-Id-Token"] = req.body.token;
+
 	if (req.body.restType === "POST") {
 		try {
-			const response = await axios.post(req.body.url, req.body.data);
-			if (response.status === 200) {
-				let responseData = response.data.accesstoken;
-				res.status(200).send(responseData);
+			const response = await axios.post(url, req.body.data);
+			if (req.body.url.includes("/user/login")) {
+				let responseData = response.data.id_token;
+				let responseRole = response.data.hmi_privileges.role;
+				res.status(200).send({ responseData, responseRole });
 			} else {
-				res.status(400).send("Failed to post!");
+				res.status(200).send(response.data);
 			}
 		} catch (err) {
-			console.log(err);
+			console.log("----------------------------------------");
+			console.log(err.response.data);
 			res.status(404).send(err);
 		}
-
-		if (req.body.restType === "GET") {
-			try {
-				const res = await axios.get(req.body.url);
-				if (res.status === 200) {
-					res.status(200).send();
-					console.log(res);
-				}
-			} catch (err) {
-				res.status(500).send(err);
-			}
+	} else if (req.body.restType === "GET") {
+		// axios.defaults.headers["X-Id-Token"] = req.body.token;
+		// console.log(axios.defaults);
+		try {
+			const response = await axios.get(req.body.url);
+			res.status(200).send(response.data);
+		} catch (err) {
+			console.log("-----------------------------------------");
+			console.log(err);
+			res.status(500).send(err);
+		}
+	} else if (req.body.restType === "DELETE") {
+		try {
+			const response = await axios.delete(url);
+			res.status(200).send();
+		} catch (err) {
+			console.log("-------------");
+			console.log(err);
 		}
 	}
 };
+
+//Admin - Organisations
+app.get("/v1/organisation");
+
+app.post("/v1/organisation/link");
+
+app.delete("/v1/organisation/link");
+
+//Admin - Users
+app.post("/v1/user/");
+
+app.patch("/v1/user/modify");
+
+app.get("/v1/user/:userId");
+
+app.delete("/v1/user/:userId");
+
+app.get("/v1/users/");
+
+//SuperUser - Organisations
+app.post("/v1/admin/organisation");
+
+app.delete("/v1/admin/organisation/delete/:organisationId");
 
 app.post("/api", PostDataToHydroCloud);
 
